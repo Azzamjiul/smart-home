23c62d58d359585478739b94feca1d2917129ec9
ripzo <yoga.mahottama@gmail.com>
2020-12-29 10:52:26 +0800

    revisi dasbord

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@ Dashboard
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <h5 class="card-title">Sensor Data</h5>
+                <h5 class="card-title">Current Sensor</h5>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse">
