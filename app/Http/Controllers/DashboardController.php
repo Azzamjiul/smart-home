05160f1c2d952fdc4e026556ec638b8a0bf07e42
A. Zaky H <azaky.h@gmail.com>
2021-01-04 11:34:56 +0700

    pengubahan durasi lamstat menjadi < 1 menit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,7 +26,7 @@ class DashboardController extends Controller
         $data_time = Carbon::parse($pir_data->created_at);
         $cur_time = Carbon::now();
         $time_diff = $cur_time->diffInMinutes($data_time);
-        if($time_diff < 5) {        // record terakhir kurang dari 5 menit yg lalu
+        if($time_diff < 1) {        // record terakhir kurang dari 5 menit yg lalu
             $status = 1;            // lampu menyala
         } else {
             $status = 0;            // lampu mati
